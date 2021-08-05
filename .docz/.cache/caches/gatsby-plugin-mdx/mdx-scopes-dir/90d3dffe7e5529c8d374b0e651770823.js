10bf030ec6431d56fcd0c29696bccaf7c5b90b97
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2021-08-05 11:34:40 +0530

    Removed docz cache

--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/90d3dffe7e5529c8d374b0e651770823.js
+++
@@ -1,9 +0,0 @@
-import { Playground, Props } from 'docz';
-import { Button } from "../../../../../src";
-import * as React from 'react';
-export default {
-  Playground,
-  Props,
-  Button,
-  React
-};
